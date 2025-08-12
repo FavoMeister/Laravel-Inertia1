b0ee7d37e5de6fe6af73f7951cb38af69e0500f7
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-08-11 23:07:54 -0400

    Create Category component and routes have been added and configured

---
+++ resources/js/pages/Dashboard/Category/Create.vue
@@ -0,0 +1,32 @@
+<template>
+    <form @submit.prevent="submit">
+        <label for="">Title</label>
+        <input type="text" v-model="form.title">
+        <label for="">Slug</label>
+        <input type="text" v-model="form.slug">
+
+        <button type="submit">Send</button>
+    </form>
+</template>
+
+<script>
+import { useForm } from '@inertiajs/vue3';
+
+
+export default {
+    setup() {
+        const form = useForm({
+            title: '',
+            slug:''
+        })
+        function submit() {
+            console.log(this.form);
+            
+        }
+        return {
+            submit
+        };
+    }
+}
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\CategoryController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -9,9 +10,20 @@ Route::get('/', function () {
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
+    
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 //Route::inertia('indexinertia', 'Dashboard/Post/Index');
+
+Route::middleware(
+    ['auth', 'verified'],
+)->prefix('dashboard')->group(function () {
+    Route::resource('/category', CategoryController::class);
+
+    Route::get('/', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
