98fa058b0680087a420c75fd5c282870d3b20c71
FavoMeister <93888759+FavoMeister@users.noreply.github.com>
2025-08-17 20:02:30 -0400

    ategory form has been updated

--- resources/js/pages/Dashboard/Category/Create.vue
+++ resources/js/pages/Dashboard/Category/Create.vue
@@ -1,9 +1,11 @@
 <template>
     <AppLayout>
+        
         <div class="px-4 py-6 max-w-xl">
-            <form @submit.prevent="submit">
+            <HeadingSmall title="Category information" description="Create new categories" />
+            <form class="space-y-6" @submit.prevent="submit">
                 <div class="grid gap-2">
-                <label for="">Title</label>
+                <Label>Title</Label>
                 <!-- <input type="text" v-model="form.title"> -->
                 <Input type="text" v-model="form.title" placeholder="Title" required></Input>
                 <!-- <div v-if="errors.title">
@@ -11,7 +13,7 @@
                 </div> -->
                 <InputError :message="errors.title"></InputError>
 
-                <label for="">Slug</label>
+                <Label for="">Slug</Label>
                 <!-- <input type="text" v-model="form.slug"> -->
                 <Input type="text" v-model="form.slug" placeholder="Slug" required />
                 <!-- <div v-if="errors.slug">
@@ -23,7 +25,7 @@
                 <!-- {{ form.errors }} -->
 
                 <!-- <button type="submit" class="btn btn-primary">Send</button> -->
-                <Button :disabled="form.processing" type="submit"></Button>
+                <Button :disabled="form.processing" type="submit">Send</Button>
                 </div>
             </form>
         </div>
@@ -31,20 +33,28 @@
 </template>
 
 <script>
-import InputError from '@/components/InputError.vue';
-import { Button } from '@/components/ui/button';
-import Input from '@/components/ui/input/Input.vue';
-import AppLayout from '@/layouts/AppLayout.vue';
 import { useForm } from '@inertiajs/vue3';
 
+import AppLayout from '@/layouts/AppLayout.vue';
+
+import InputError from '@/components/InputError.vue';
+import Input from '@/components/ui/input/Input.vue';
+import Label from '@/components/ui/label/Label.vue';
+
+import { Button } from '@/components/ui/button';
+import HeadingSmall from '@/components/HeadingSmall.vue';
 
 export default {
     props: {
         errors: Object
     },
     components: {
+        AppLayout,
         InputError,
-        Input
+        Input,
+        Button,
+        Label,
+        HeadingSmall
     },
     setup() {
         Input
